d5699ac2c48c98009286a9deb136dd6b31a73c0a
mahendrabp <chipxitro@gmail.com>
2019-11-20 10:27:53 +0700

    Merge: add asset

--- App.js
+++ App.js
@@ -8,6 +8,12 @@ import {createStackNavigator} from 'react-navigation-stack';
 // import RegisterScreen from './src/screens/user/RegisterScreen';
 import MenuTabs from './src/screens/menu';
 import MyHeader from './src/screens/header/Header';
+import LoginScreen from './src/screens/user/LoginScreen';
+import SplashScreen from './src/screens/user/SplashScreen';
+import IntroScreen from './src/screens/IntroScreen';
+import HelpScreen from './src/screens/user/HelpScreen';
+import OTP from './src/screens/user/OTP';
+import SecurityCode from './src/screens/user/SecurityCode';
 
 function App() {
   return (
@@ -18,83 +24,74 @@ function App() {
   );
 }
 
-const MainNavigator = createStackNavigator(
-  {
-    // RegisterScreen: {
-    //   screen: RegisterScreen,
-    //   navigationOptions: {
-    //     headerStyle: {
-    //       backgroundColor: '#534090',
-    //     },
-    //   },
-    // // },
+const MainNavigator = createStackNavigator({
+  // RegisterScreen: {
+  //   screen: RegisterScreen,
+  //   navigationOptions: {
+  //     headerStyle: {
+  //       backgroundColor: '#534090',
+  //     },
+  //   },
+  // // },
 
-    // SplashScreen: {
-    //   screen: SplashScreen,
-    //   navigationOptions: {
-    //     header: null,
-    //   },
-    // },
-    // LoginScreen: {
-    //   screen: LoginScreen,
-    //   navigationOptions: {
-    //     header: null,
-    //   },
-    // },
-    MenuTabs: {screen: MenuTabs},
+  // SplashScreen: {
+  //   screen: SplashScreen,
+  //   navigationOptions: {
+  //     header: null,
+  //   },
+  // },
+  // LoginScreen: {
+  //   screen: LoginScreen,
+  //   navigationOptions: {
+  //     header: null,
+  //   },
+  // },
+  //     MenuTabs: {screen: MenuTabs},
+  //   },
+  //   {
+  //     initialRouteName: 'MenuTabs',
+  //     headerMode: 'none',
+  //   },
+  // );
+
+  // const StackNavigator = createStackNavigator({
+  SplashScreen: {
+    screen: SplashScreen,
+    navigationOptions: {
+      header: null,
+    },
   },
-  {
-    initialRouteName: 'MenuTabs',
-    headerMode: 'none',
+  IntroScreen: {
+    screen: IntroScreen,
+    navigationOptions: {
+      header: null,
+    },
   },
-);
-
-// import LoginScreen from './src/screens/user/LoginScreen';
-// import SplashScreen from './src/screens/user/SplashScreen';
-// import IntroScreen from './src/screens/IntroScreen';
-// import HelpScreen from './src/screens/user/HelpScreen';
-// import OTP from './src/screens/user/OTP';
-// import SecurityCode from './src/screens/user/SecurityCode';
-
-// const StackNavigator = createStackNavigator({
-//   SplashScreen: {
-//     screen: SplashScreen,
-//     navigationOptions: {
-//       header: null,
-//     },
-//   },
-//   IntroScreen: {
-//     screen: IntroScreen,
-//     navigationOptions: {
-//       header: null,
-//     },
-//   },
-//   HelpScreen: {
-//     screen: HelpScreen,
-//     navigationOptions: {
-//       header: null,
-//     },
-//   },
-//   LoginScreen: {
-//     screen: LoginScreen,
-//     navigationOptions: {
-//       header: null,
-//     },
-//   },
-//   SecurityCode: {
-//     screen: SecurityCode,
-//     navigationOptions: {
-//       header: null,
-//     },
-//   },
-//   OTP: {
-//     screen: OTP,
-//     navigationOptions: {
-//       header: null,
-//     },
-//   },
-// });
-// >>>>>>> origin/interface_intro_to_app
+  HelpScreen: {
+    screen: HelpScreen,
+    navigationOptions: {
+      header: null,
+    },
+  },
+  LoginScreen: {
+    screen: LoginScreen,
+    navigationOptions: {
+      header: null,
+    },
+  },
+  SecurityCode: {
+    screen: SecurityCode,
+    navigationOptions: {
+      header: null,
+    },
+  },
+  OTP: {
+    screen: OTP,
+    navigationOptions: {
+      header: null,
+    },
+  },
+});
 
 const AppContainer = createAppContainer(MainNavigator);
 
